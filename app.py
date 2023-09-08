43b10fcc4f3b296080725f27e98e1ab6db8224b2
millow <millow@github.com>
2023-09-08 01:38:47 -0500

    5 ultimas rutas, mas 2 primeras

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 from fastapi import FastAPI
-# import mysql.connector
-# from fastapi.encoders import jsonable_encoder
+import mysql.connector
+from fastapi.encoders import jsonable_encoder
 from enum import Enum
 from datetime import date
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel
 
 app = FastAPI()
 
-# mydb = mysql.connector.connect(
-#   host = "localhost",
-#   user = "root",
-#   password = "",
-#   database  = ""
-# )
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="learnmot"
+)
 
 # class SelectRole(str, Enum):
 #     Admin = 'admin'
@@ -23,40 +23,214 @@ app = FastAPI()
 #     Male = 'male'
 #     Female = 'female' 
 
-class Users(BaseModel):
-    name: str = Field(min_length=1, max_length=10)
-    last_name: str = Field(min_length=1, max_length=10)
-    role: str 
+class User(BaseModel):
+    name: str 
+    last_name: str 
     sex: str
+    role: str 
     email: str
-    password: str = Field(min_length=1, max_length=10)
+    password: str 
 
-class Rol(BaseModel):
-    name: str = Field(min_length=1, max_length=25   )
-    description: str = Field(min_length=1, max_length=25)
+class Role(BaseModel):
+    name: str 
+    description: str 
 
 class Attribute(BaseModel):
     name: str 
     description: str
 
 class Subject(BaseModel):
-    name: str = Field(min_length=1, max_length=10)
-    name_code: str = Field(min_length=1, max_length=10, gt=1, lt=11)
-    description: str = Field(min_length=1, max_length=200)
+    name: str 
+    name_code: str
+    description: str 
 
 class Module(BaseModel):
-    name: str = Field(min_length=1, max_length=25)
-    descripion: str = Field(min_length=1, max_length=25)
+    name: str 
+    descripion: str 
 
 class AcademicLoad(BaseModel):
-    topic: str = Field(min_length=1, max_length=15)
-    description: str = Field(min_length=1, max_length=200)
+    topic: str 
+    description: str 
     since_date: date 
     until_date: date
 
 class Observation(BaseModel):
-    Observations: str = Field(max_legth=600)
+    Observations: str 
+
+# {
+#   "name": "samuel",
+#   "last_name": "dulce",
+#   "sex": "male",
+#   "role": "estudiante",
+#   "email": "samuel@gmail.com",
+#   "password": "samuel123"
+# }
 
 @app.get("/")
 def root():
     return {"message": "hello sam"}
+
+@app.get("/listusers")
+def listusers():
+    try:
+        data = mydb.data()
+        data.execute("SELECT * FROM users")
+        result = data.fetchall()
+        payload = []
+        content = {} 
+        for res in result:
+            content={
+                'id':res[0],
+                'name':res[1],
+                'last_name':res[2],
+                'sex':res[3],
+                'role':res[4],
+                'email':res[5],
+                'password':res[6],
+            }
+            payload.append(content)
+            content = {}
+        print(payload)
+        json_data = jsonable_encoder(payload)            
+        return {"result": json_data}
+    except (Exception) as error:
+        return {"error":error}
+
+@app.post("/insertuser")
+def insertuser(newuser: User):
+    try:
+        name = newuser.name
+        last_name = newuser.last_name
+        sex = newuser.sex
+        role = newuser.role
+        email = newuser.email
+        password = newuser.password
+        data = mydb.data() 
+        data.execute("INSERT INTO users(name,last_name,sex,role,email,password) VALUES(%s,%s,%s,%s,%s,%s)",(name,last_name,sex,role,email,password))
+        mydb.commit()
+        data.close()
+        return {"info":"User create successful."}
+    except Exception as error:
+        return {"error":error}
+
+@app.delete("users/deletelike/{name}")
+def deletelike(name: str):
+    try:
+        data = mydb.data()
+        data.execute("DELETE FROM users WHERE name like = '%s'",(name,))
+        mydb.commit()
+        data.close()
+        return {"info":"User removed successful."}
+    except Exception as error:
+        return {"result":error}
+
+@app.get("users/userscount")
+def userscount():
+    try:
+        data = mydb.data()
+        data.execute("SELECT COUNT(role) FROM users")
+        result = data.fetchall()
+        payload = []
+        content = {}
+        for res in result:
+            content={
+                'role':res[0],
+                # 'name':res[1],
+                # 'las_name':res[2],
+                # 'sex':res[3],
+            }
+        payload.append(content)
+        content = {}
+        print(payload)
+        json_data = jsonable_encoder(payload)            
+        return {"result": json_data}
+    except Exception as error:
+        return {"result":error}
+    
+# @app.get("users/usersum")
+# def usersum():
+#     try:
+#         data = mydb.data()
+#         data.execute("SELECT SUM(role) FROM subject")
+#         result = data.fetchall()
+#         payload = []
+#         content = {}
+#         for res in result:
+#             content={
+#                 'role':res[0],
+#                 # 'name':res[1],
+#                 # 'las_name':res[2],
+#                 # 'sex':res[3],
+#             }
+#         payload.append(content)
+#         content = {}
+#         print(payload)
+#         json_data = jsonable_encoder(payload)            
+#         return {"resultado": json_data}
+#     except Exception as error:
+#         return {"resultado":error}
+    
+@app.get("users/docente/subject/{id}")
+def getTeacherBySubject(id: int):
+    try:
+        data = mydb.data()
+        data.execute("""
+            SELECT u.id, u.name, u.last_name, u.role, al.topic
+            FROM academic_load al
+            JOIN users u 
+            on u.id = al.teacher_id
+            WHERE al.id = %s
+            ORDER BY al.topic DESC 
+        """,(id,))
+        result = data.fetchall()
+        payload = []
+        content = {} 
+        for res in result:
+            content={
+                'id':res[0],
+                'name':res[1],
+                'last_name':res[2],
+                'role':res[3],
+                'topic':res[4],
+            }
+            payload.append(content)
+            content = {}
+        print(payload)
+        json_data = jsonable_encoder(payload)            
+        return {"result": json_data}
+    except (Exception) as error:
+        return {"result":error}
+    
+@app.get("users/subject/observation/{id}/{topic}")
+def getSubjectAndObservation(id: int, topic: str):
+    try:
+        data = mydb.data()
+        data.execute("""
+            SELECT u.id, u.name, u.last_name, u.role, al.topic, o.observation_desc
+            FROM academic_load al
+            JOIN users u 
+            on al.teacher_id = u.id
+            JOIN observation o 
+            on o.academic_load_id = al.id
+            WHERE al.id = %s
+            AND al.topic = %s
+        """,(id, topic,))
+        result = data.fetchall()
+        payload = []
+        content = {} 
+        for res in result:
+            content={
+                'id':res[0],
+                'name':res[1],
+                'last_name':res[2],
+                'role':res[3],
+                'topic':res[4],
+                'observation_desc':res[5],
+            }
+            payload.append(content)
+            content = {}
+        print(payload)
+        json_data = jsonable_encoder(payload)            
+        return {"result": json_data}
+    except (Exception) as error:
+        return {"result":error}
